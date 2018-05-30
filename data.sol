d6faced35593093ea5ef19d988e8763b8ead99c3
Alin <rcijov@gmail.com>
2018-05-30 15:27:22 -0400

    Updating the address by the msg.sender

--- data.sol
+++ data.sol
@@ -30,57 +30,59 @@ contract Data{
     
     mapping (address=>tree) public users;
     
-    function resetData(address _user) public returns (bool success){
+    function resetData() public returns (bool success){
         
-        users[_user].root = empty;
-        users[_user].tree = new Tree();
-        users[_user].length = 0;
+        addUser();
         
         return true;
     }
     
-    function addUser(address _user) public
-    {
-        users[_user].tree = new Tree();
+    function addUser() public returns (bool success){
+        
+        users[msg.sender].tree = new Tree();
+        users[msg.sender].root = empty;
+        users[msg.sender].length = 0;
+        
+        return true;
     }
     
-    function addData(uint256 _data,address _user) public returns (bool success) {  
+    function addData(uint256 _data) public returns (bool success) {  
         
         bytes32 leaf    = keccak256(abi.encodePacked(_data));   
-        bytes32 oldRoot = getUserRoot(_user);
+        bytes32 oldRoot = getUserRoot();
         bytes32 newRoot = hashTheTwo(leaf, oldRoot);
         
-        if(!users[_user].exists){ users[_user].tree = new Tree(); users[_user].exists = true; }
+        if(!users[msg.sender].exists){ addUser(); }
         
-        users[_user].tree.setLeafAndRoot(leaf,newRoot);
-        users[_user].root = newRoot;
-        users[_user].length += 1;
+        users[msg.sender].tree.setLeafAndRoot(leaf,newRoot);
+        users[msg.sender].root = newRoot;
+        users[msg.sender].length += 1;
 
         return true;
     }
     
-    function getRoot(uint256 _leafData,address _user) constant public returns (bytes32 root) { 
+    function getRoot(uint256 _leafData) constant public returns (bytes32 root) { 
         
         bytes32 leaf = keccak256(abi.encodePacked(_leafData));                                            
-        return users[_user].tree.getLeafAndRoot(leaf);
+        return users[msg.sender].tree.getLeafAndRoot(leaf);
     }
     
-    function getUserRoot(address _user) constant public returns (bytes32 root) {      
-        return users[_user].root;
+    function getUserRoot() constant public returns (bytes32 root) {      
+        return users[msg.sender].root;
     }
 
     function hashTheTwo(bytes32 _a, bytes32 _b) pure private returns (bytes32 hashed) {         
         return keccak256(abi.encodePacked(_a, _b));
     }
     
-    function checkDataIntegrity(uint256[] _data,address _user) constant public returns (bool complete) { 
+    function checkDataIntegrity(uint256[] _data) constant public returns (bool complete) { 
          
         bytes32 oldRoot = empty;                                               
         for (uint i = 0; i < _data.length; i++) {         
             bytes32 data = keccak256(abi.encodePacked(_data[i]));          
             bytes32 root = hashTheTwo(data, oldRoot);
             
-            if(root == getRoot(_data[i], _user)){         
+            if(root == getRoot(_data[i])){         
                 oldRoot = root;
                 continue;
             }else{
@@ -88,7 +90,7 @@ contract Data{
             }
         }        
 
-        if (oldRoot == getUserRoot(_user)){
+        if (oldRoot == getUserRoot()){
             return true;
         }else{
             return false;
